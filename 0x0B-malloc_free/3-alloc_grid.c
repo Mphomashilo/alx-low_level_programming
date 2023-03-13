7b7160b4253048987218d6c100113c77b96bc70d
Mpho <mphomashilo.mc@gmail.com>
2023-03-13 11:50:28 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 
 /**
- * alloc_grind - ...
- * @width: ...
- * @height: ...
- * Return: ...
+ * alloc_grind - creates a two-dimensional array of integers
+ * @width: the width of the grid
+ * @height: the height of the grid
+ * Return: If successful, a pointer to the newly allocated grid
  */
 
-int **alloc_grid(int width, int height)
+int **alloc_grid(int width, int height);
+
+/* function code here */
 
 {
 	int i, j, k, l;
